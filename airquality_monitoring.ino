cc134af230d20fb47cd44d277dcd558a002dd4c1
SavioN200 <davionsebastianbabu@gmail.com>
2025-01-07 08:28:43 +0530

    Update airquality_monitoring.ino

--- airquality_monitoring.ino
+++ airquality_monitoring.ino
@@ -1,3 +1,89 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET 4
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define sensor A0
+#define DHT11PIN 2
+#define DHTTYPE DHT11
+#define BUZZER_PIN 7
+
+int gasLevel = 0;
+String quality = "";
+DHT dht(DHT11PIN, DHTTYPE);
+
+// Hysteresis thresholds
+int goodThreshold = 151;
+int poorThreshold = 200;
+int veryBadThreshold = 300;
+int toxicThreshold = 500;
+
+void sendSensor() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 43);
+  display.print("Temp  :");
+  display.setCursor(80, 43);
+  display.print(t, 1);
+  display.print("C");
+  display.setCursor(0, 56);
+  display.print("RH    :");
+  display.setCursor(80, 56);
+  display.print(h, 1);
+  display.print("%");
+}
+
+void air_sensor() {
+  gasLevel = analogRead(sensor);
+
+  if (gasLevel < goodThreshold) {
+    quality = "  GOOD!";
+    digitalWrite(BUZZER_PIN, LOW);
+  } else if (gasLevel >= goodThreshold && gasLevel < poorThreshold) {
+    quality = "  Poor!";
+    digitalWrite(BUZZER_PIN, LOW);
+  } else if (gasLevel >= poorThreshold && gasLevel < veryBadThreshold) {
+    quality = "Very bad!";
+    digitalWrite(BUZZER_PIN, LOW);
+  } else if (gasLevel >= veryBadThreshold && gasLevel < toxicThreshold) {
+    quality = "Toxic!";
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(500);
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(500);
+  } else {
+    quality = " Toxic";
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(500);
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(500);
+  }
+
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(1, 5);
+  display.print("Air Quality:");
+  display.setTextSize(1);
+  display.setCursor(5, 23);
+  display.print(gasLevel);
+  display.setCursor(20, 23);
+  display.print(quality);
+}
+
 void setup() {
   Serial.begin(9600);
   pinMode(sensor, INPUT);
@@ -44,4 +130,13 @@ void setup() {
   }
 
   Serial.println("MQ135 sensor is ready.");
+}
+
+
+void loop() {
+  display.clearDisplay();
+  air_sensor();
+  sendSensor();
+  display.display();
+  delay(500);
 }
